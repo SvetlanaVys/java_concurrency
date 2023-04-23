ad205912d517dcb4363f02608143ad6c288d3f04
Svitlana <vyskrebentseva.svetlana@gmail.com>
2023-04-23 19:30:57 +0300

    Initial commit

---
+++ src/test/java/com/svysk/concurrency/AppTest.java
@@ -0,0 +1,19 @@
+package com.svysk.concurrency;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    void testApp()
+    {
+        assertTrue( true );
+    }
+}
